3f58d12931ab756e09775d6c00ce1388e614a6bc
shahdahmedd <shahd.ahmed15@msa.edu.eg>
2026-05-03 11:40:56 +0300

    Initial commit - Padel Learning System

---
+++ TUIO11_NET-master/UserData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TuioDemo
+{
+    public class UserData
+    {
+        public string UserId { get; set; }
+        public string BluetoothId { get; set; }    
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public int Age { get; set; }
+        public string Level { get; set; }
+    }
+}
---
+++ TUIO11_NET-master/WordItemData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TuioDemo
+{
+    internal class WordItemData
+    {
+        public string Word { get; set; }
+        public string Meaning { get; set; }
+        public string Example { get; set; }
+        public string ImageName { get; set; }
+    }
+}
